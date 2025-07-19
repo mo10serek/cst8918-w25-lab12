9739fe8952f226d9cdce7561ea8e38c24e609082
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-07-19 16:42:59 -0400

    set up backend terraform application

---
+++ infra/tf-backend/main.tf
@@ -0,0 +1,54 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  # Leave the features block empty to accept all defaults
+  features {}
+}
+
+provider "cloudinit" {
+  # Configuration options
+}
+
+output "arm_access_key" {
+  value     = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "mo10serek-githubactions-rg"
+  location = "westus3"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = "mo10serekgithubactions" # must be globally unique, 3-24 lowercase letters/numbers
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+# Storage Container
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "container"
+}
