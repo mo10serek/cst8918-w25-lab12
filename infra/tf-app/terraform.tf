872f1b939a1dfcf83b05ba44cc778cac6a864706
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-07-19 16:43:11 -0400

    set up frontend application

---
+++ infra/tf-app/terraform.tf
@@ -0,0 +1,37 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "mo10serek-githubactions-rg"
+    storage_account_name = "mo10serekgithubactions"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
+    use_oidc             = true
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  # Leave the features block empty to accept all defaults
+  features {}
+  use_oidc = true
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "mo10serek-a12-rg"
+  location = "westus3"
+}
