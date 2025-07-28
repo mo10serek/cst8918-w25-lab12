c81d67fbef80d8dcafad1b67d79b0d3e654b7151
danielabouassalyy <abou0344@algonquinlive.com>
2025-07-28 10:21:22 -0400

    chore: finalize Terraform config and lockfiles for Steps 3+4

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,12 +1,10 @@
-# Configure the Terraform runtime requirements.
 terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
-    # Azure Resource Manager provider and version
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.64.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
@@ -19,19 +17,21 @@ terraform {
     storage_account_name = "mo10serekgithubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
-    use_oidc             = true
   }
 }
 
-# Define providers and their config params
 provider "azurerm" {
-  # Leave the features block empty to accept all defaults
   features {}
-  use_oidc = true
+  use_oidc                     = true
+  skip_provider_registration   = true
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "mo10serek-a12-rg"
   location = "westus3"
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group created"
+  value       = azurerm_resource_group.rg.name
 }
