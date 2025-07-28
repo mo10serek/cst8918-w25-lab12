dff042b5cbc249083424e39a14276836a2ba6291
danielabouassalyy <abou0344@algonquinlive.com>
2025-07-28 10:49:25 -0400

    style: format Terraform code with terraform fmt

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -18,8 +18,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc                     = true
-  skip_provider_registration   = true
+  use_oidc                   = true
+  skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
